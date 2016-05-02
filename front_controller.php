be8ffa855a74e8e7cf760ae6d95bc77ce88b0763
Cristofer <Cristofer.dev@gmail.com>
2016-05-02 13:10:44 -0300

    Validacion Instancias y metodos en FrontController
    Asegura que solo se hara un require a un archivo
    existente y luego una instancia solo si la clase
    existe. Lo mismo para los metodos.
    	modified:   front_controller.php

--- front_controller.php
+++ front_controller.php
@@ -5,15 +5,20 @@ $solicitud = $_SERVER['REQUEST_URI'];
 $array = explode("/", $solicitud);
 
 $modulo = $array[1];
-$recurso = $array[2];
-$arg = $array[3];
+$recurso = (isset($array[2])) ? $array[2] : '' ;
+$arg = (isset($array[3])) ? $array[3] : 0 ;
 
-require_once "modules/".strtolower($modulo).".php";
+settype($arg, 'int');
 
+$file = "modules/".strtolower($modulo).".php";
+
+if (file_exists($file)) require_once $file;
 $c = ucwords($modulo)."Controller";
+if (class_exists($c)) $controller = new $c();
+if (method_exists($c, agregar) $controller->agregar();
 
-$controller = new $c();
 
-$controller->agregar();
+
+
 
 ?>
